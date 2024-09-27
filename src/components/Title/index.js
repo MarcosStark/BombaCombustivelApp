741caa9857a53201351df047322c209b05a3a372
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2024-09-27 16:15:39 -0300

    Update index.js

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,11 +5,6 @@ export default function Title(){
     return(
         <View>
             <Text>Cálculo do combustível</Text>
-            {/*<TextInput value={baseValue} onChange={(e) => setBaseValue(e.target.value)} placeholder='Valor da base...'/>
-            <TextInput value={heightValue} onChange={(e) => setHeightValue(e.target.value)}placeholder='Valor da altura...'/>
-            <Text value={result1} onChange={(e) => setResult1(e.target.value)}></Text>
-            <Button title='Calcular'/>
-            <StatusBar style="auto" />*/}
         </View>
     )
 }
